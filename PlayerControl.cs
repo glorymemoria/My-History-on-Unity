d456b21438d55331f425e1ee6bed7980ab7fbe2f
Yang Hong <81492857+glorymemoria@users.noreply.github.com>
2022-01-14 13:38:51 +0000

    PlaneGamePart7-3
    contain how to change the scenes, move a stuff by certain trails, and how to destroy an item while be collidered

---
+++ PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Vector3 leftPos = new Vector3(-4, -3, 0);
+    Vector3 midPos = new Vector3(0, -3, 0);
+    Vector3 rightPos = new Vector3(4, -3, 0);
+
+    void Start()
+    {
+        transform.position = midPos;
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (transform.position == midPos)
+            {
+                transform.position = leftPos;
+            }else if (transform.position == rightPos)
+            {
+                transform.position = midPos;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (transform.position == midPos)
+            {
+                transform.position = rightPos;
+            }else if (transform.position == leftPos)
+            {
+                transform.position = midPos;
+            }
+        }
+    }
+}
---
+++ enemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        int speed = 5;
+
+        void FixedUpdate()
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+
+            if (transform.position.y < -6)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 1);
+    }
+}
